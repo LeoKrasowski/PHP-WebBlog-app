5052b0e9ad80c6c6d2a7a3eebbf971a20e1e28b3
LeoKrasowski <leokrasowski@gmail.com>
2024-08-12 15:37:42 +0200

    detail page css improvments

--- detail.php
+++ detail.php
@@ -12,21 +12,9 @@
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">sharing</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">export</button>
-          </div>
-          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
-            <svg class="bi"><use xlink:href="#calendar3"></use></svg>
-            This week
-          </button>
-        </div>
-      </div>
+        
+      <div class="section"><h3>Section title</h3></div>
 
-      <canvas class="my-4 w-100" id="myChart" width="792" height="334" style="display: block; box-sizing: border-box; height: 267px; width: 634px;"></canvas>
-
-      <h2>Section title</h2>
       <div class="table-responsive small">
         <table class="table table-striped table-sm">
           <thead>
@@ -141,9 +129,6 @@
         </table>
       </div>
     </main>
-
-
-
 
     <?php require "blocks/footer.php" ?>
 </body>
