0de04bddd3f4810bcde7c0d8b748b88a070423df
LeoKrasowski <leokrasowski@gmail.com>
2024-08-14 14:55:42 +0200

    creating function is done, relocation is mapping on the detail page

--- vendor/create.php
+++ vendor/create.php
@@ -8,3 +8,5 @@ $publisher = $_POST['publisher'];
 $year = $_POST['year'];
 
 mysqli_query($connect, "INSERT INTO `publications` (`id`, `title`, `author`, `description`, `publisher`, `year`) VALUES (NULL, '$title', '$author', '$description', '$publisher', '$year')");
+
+header('Location: ../detail.php');
