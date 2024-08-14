5b73e749f3c89f4bfb4362e2268b22b4be572613
LeoKrasowski <leokrasowski@gmail.com>
2024-08-14 10:52:22 +0200

    table is created, ability to look db instance

--- detail.php
+++ detail.php
@@ -13,95 +13,47 @@
 <body>
     <?php require "blocks/header.php" ?>
 
-    <pre>
-      <?php
-        $publications = mysqli_query($connect, "SELECT * FROM `publications`");
-        $publications = mysqli_fetch_all($publications);
-        print_r($publications);
-      ?>
-    </pre>
+    
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Publications</h1>
         
       <div class="table-responsive small">
         <table class="table table-striped table-sm">
-          <thead>
+        <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Author</th>
+                <th scope="col">Description</th>
+                <th scope="col">Publisher</th>
+                <th scope="col">Year</th>
+              </tr>
+            </thead>
+
+        <?php
+        $publications = mysqli_query($connect, "SELECT * FROM `publications`");
+        $publications = mysqli_fetch_all($publications);
+        foreach ($publications as $publication) {
+            ?>
+
             <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Title</th>
-              <th scope="col">Author</th>
-              <th scope="col">Description</th>
-              <th scope="col">Publisher</th>
-              <th scope="col">Year</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>Data</td>
-              <td>Random</td>
-              <td>Enriches</td>
-              <td>Table</td>
-            </tr>
-            <tr>
-              <td>1,002</td>
-              <td>Enriches</td>
-              <td>Magnificent</td>
-              <td>Design</td>
-              <td>Coordination</td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>Random</td>
-              <td>Rich</td>
-              <td>Value</td>
-              <td>Useful</td>
-            </tr>
-            <tr>
-              <td>1,004</td>
-              <td>Random</td>
-              <td>Rich</td>
-              <td>Value</td>
-              <td>Useful</td>
-            </tr>
-            <tr>
-              <td>1,005</td>
-              <td>Random</td>
-              <td>Rich</td>
-              <td>Value</td>
-              <td>Useful</td>
-            </tr>
-            <tr>
-              <td>1,006</td>
-              <td>Random</td>
-              <td>Rich</td>
-              <td>Value</td>
-              <td>Useful</td>
-            </tr>
-            <tr>
-                <td>1,007</td>
-                <td>Data</td>
-                <td>Random</td>
-                <td>enrich</td>
-                <td>Table</td>
-                </tr>
-            <tr>
-            <td>1,008</td>
-              <td>enrich</td>
-              <td>Incandescent</td>
-              <td>Design</td>
-              <td>Format</td>
-            </tr>
-            <tr>
-            <td>1,009</td>
-              <td>enrich</td>
-              <td>Incandescent</td>
-              <td>Design</td>
-              <td>Format</td>
-            </tr>
-            
-          </tbody>
+              <td><?= $publication[0] ?></td>
+              <td><?= $publication[1]?></td>
+              <td><?= $publication[2] ?></td>
+              <td><?= $publication[3] ?></td>
+              <td><?= $publication[4] ?></td>
+              <td><?= $publication[5] ?></td>
+
+          <?php
+        }
+        
+        
+        
+        
+      ?>
+          
+         
         </table>
       </div>
     </main>
